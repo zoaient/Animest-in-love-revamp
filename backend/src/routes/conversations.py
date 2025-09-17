4b069760a058b096ad7d2a1d1a3f36ecf0c072b3
e0297u <zoe.le-gleut@telecomnancy.eu>
2025-09-17 12:03:08 +0200

    get history fait, ca avance biengue

--- backend/src/routes/conversations.py
+++ backend/src/routes/conversations.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 from typing import List
-from src.db import messages_collection
+from src.db import messages_collection , gamestates_collection
 from src.models import Message
 
 router = APIRouter()
 
-@router.get("/conversation", response_model=List[Message])
+@router.get("/next_messages", response_model=List[Message]) #Ici, tout envoyer en fonction de la branche selectionnée, la fonction va de choix en choix , get le gamestate aussi
 def get_next_messages():
     chatroom_text = list(messages_collection.find({}))
-    all_messages=chatroom_text[0]["messages"]
+    all_messages=chatroom_text[0]["messages"]  
     game_state =3
     Player_found = False
     displayed_messages = []
@@ -22,3 +22,32 @@ def get_next_messages():
         
     print(displayed_messages)
     return displayed_messages
+
+def get_chatroom_messages(chatroom_id):
+    return list(messages_collection.find({"id":chatroom_id})[0]["messages"])
+
+@router.get("/history/{player_name}/{channel_name}", response_model=List[Message])
+def get_history(channel_name,player_name):
+    player_gamestate = list(gamestates_collection.find({"name": player_name}))
+    player_history =player_gamestate[0]["history"]
+    messages_history=[]
+
+    for history in player_history:
+        messages =get_chatroom_messages(history["chatroom_id"])
+        choices =history["choices"]
+        current_branch = choices[0]
+        branch_counter = 0
+        has_branch_started =False
+        for message in messages:
+            if message["channel"] ==channel_name:
+                if  message["branch"]==current_branch or not message["branch"]:
+                    messages_history.append(message)
+                    has_branch_started=True
+                elif has_branch_started:
+                    has_branch_started=False
+                    if(branch_counter+1<len(choices)):
+                        branch_counter+=1
+                        current_branch=choices[branch_counter]
+    return messages_history
+
+
